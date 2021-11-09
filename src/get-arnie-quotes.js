31f1f91b8b23219d0f6f667fb32fd343da8bcf51
Jake Owen <jakeowen.ex@gmail.com>
2021-11-09 17:44:29 +1100

    Add jsdocs

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,16 @@
 const { httpGet } = require('./mock-http-interface');
 
+/**
+ * @typedef {{body: string, status: number}} apiResponse
+ */
 const SUCCESS_STATUS_CODE = 200;
 const ARNIE_QUOTE_KEY = 'Arnie Quote';
 const FAILURE_KEY = 'FAILURE';
+
+/**
+ * @param apiResponse {apiResponse} response from httpGet
+ * @return {Object.<string, string>} arnie quote or failure message
+ */
 const transformApiResponseToQuote = (apiResponse) => {
   const body = JSON.parse(apiResponse.body);
   const message = body.message;
@@ -13,8 +21,16 @@ const transformApiResponseToQuote = (apiResponse) => {
   }
 }
 
+/**
+ * @param urls {string[]} all urls to call
+ * @return {apiResponse[]} result from each api call
+ */
 const batchCallUrls = async (urls) => await Promise.all(urls.map((url) => httpGet(url)));
 
+/**
+ * @param urls {string[]} all urls to call for arnie quotes
+ * @return {Object.<string, string>[]} array of quotes or failure messages
+ */
 const getArnieQuotes = async (urls) => {
   const quotes = [];
   const apiResults = await batchCallUrls(urls);
