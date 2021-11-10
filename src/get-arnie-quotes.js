d385a9c7d9a348791afaf14d0fa0630dd4967db6
Jake Owen <jakeowen.ex@gmail.com>
2021-11-10 13:07:41 +1100

    Fix/Return use ternary for cleaner code

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -14,11 +14,9 @@ const FAILURE_KEY = 'FAILURE';
 const transformApiResponseToQuote = (apiResponse) => {
   const body = JSON.parse(apiResponse.body);
   const message = body.message;
-  if (apiResponse.status === SUCCESS_STATUS_CODE) {
-    return { [ARNIE_QUOTE_KEY]: message };
-  } else {
-    return { [FAILURE_KEY]: message };
-  }
+  return apiResponse.status === SUCCESS_STATUS_CODE
+      ? { [ARNIE_QUOTE_KEY]: message }
+      : { [FAILURE_KEY]: message }
 }
 
 /**
